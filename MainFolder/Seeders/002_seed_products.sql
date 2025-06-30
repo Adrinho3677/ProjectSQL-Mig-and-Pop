5a8276c37eb16aff60e1ac751e0280bad0e10222
Adrinho3677 <162354892+Adrinho3677@users.noreply.github.com>
2025-06-30 13:28:19 -0300

    Create 002_seed_products.sql

---
+++ MainFolder/Seeders/002_seed_products.sql
@@ -0,0 +1,6 @@
+-- Inserção de produtos fictícios
+INSERT INTO produtos (nome, preco, estoque) VALUES
+('Camiseta Branca', 49.90, 100),
+('Calça Jeans', 129.99, 50),
+('Tênis Esportivo', 239.90, 30),
+('Boné Azul', 39.00, 20);
