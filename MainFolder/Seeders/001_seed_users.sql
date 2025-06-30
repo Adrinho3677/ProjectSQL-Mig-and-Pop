b20214c4e44c648199aee1027bf1fd6e72952a9c
Adrinho3677 <162354892+Adrinho3677@users.noreply.github.com>
2025-06-30 13:27:47 -0300

    Create 001_seed_users.sql

---
+++ MainFolder/Seeders/001_seed_users.sql
@@ -0,0 +1,5 @@
+-- Inserção de usuários fictícios
+INSERT INTO usuarios (nome, email) VALUES
+('João Silva', 'joao@email.com'),
+('Maria Souza', 'maria@email.com'),
+('Pedro Oliveira', 'pedro@email.com');
